5665e301fa3cd83f021c6e719eaa13edbe6034f4
Michal Kovacik <michal.kovacik@outlook.com>
2026-05-15 13:06:49 +0200

    Revert payload migrate from build, use push mode instead

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260515_105450_add_festival_active_field from './20260515_105450_add_festival_active_field';
-
-export const migrations = [
-  {
-    up: migration_20260515_105450_add_festival_active_field.up,
-    down: migration_20260515_105450_add_festival_active_field.down,
-    name: '20260515_105450_add_festival_active_field'
-  },
-];
